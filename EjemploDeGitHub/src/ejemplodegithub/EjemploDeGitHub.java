1472ca9b11a14096a7d4718f5a050f243d69e634
profesor <profesor>
2018-05-09 17:04:57 -0300

    saludo hola mundo

--- EjemploDeGitHub/src/ejemplodegithub/EjemploDeGitHub.java
+++ EjemploDeGitHub/src/ejemplodegithub/EjemploDeGitHub.java
@@ -15,7 +15,8 @@ public class EjemploDeGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //aca esta el main
+        System.out.println("hola mundo");
     }
     
 }
